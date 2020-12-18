ccfe6b8a6ab9a5529ec3b1c4e74109663d77cd9b
SeanBlend <awesome.sean.haha@gmail.com>
2020-12-17 18:42:08 -0800

    made needed stuff

--- ui/ui.py
+++ ui/ui.py
@@ -3,11 +3,3 @@ from ..constants import *
 
 pygame.init()
 
-def Main():
-    while True:
-        events = pygame.event.get()
-        keys = pygame.key.get_pressed()
-
-        pygame.display.update()
-        DISPLAY = pygame.display.set_mode(SCREEN)
-        pygame.display.set_caption("2D Minecraft Remix")
