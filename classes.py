85f8786c3df348c7af568d9ee7a9eebbf8ab5185
SeanBlend <awesome.sean.haha@gmail.com>
2020-12-17 19:27:07 -0800

    renamed and moved file

---
+++ classes.py
@@ -0,0 +1,8 @@
+import pygame
+from .constants import *
+
+pygame.init()
+
+class Toolbar:
+    TOOLSPACE_WIDTH_HEIGHT = 100
+    
--- ui/ui.py
+++
@@ -1,5 +0,0 @@
-import pygame
-from ..constants import *
-
-pygame.init()
-
