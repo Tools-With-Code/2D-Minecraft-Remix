1f07b8f1f360104761a170d24f940be5b6d02350
SeanBlend <awesome.sean.haha@gmail.com>
2020-12-17 20:39:25 -0800

    added block class

--- classes.py
+++ classes.py
@@ -1,8 +1,16 @@
 import pygame
-from .constants import *
 
 pygame.init()
 
-class Toolbar:
-    TOOLSPACE_WIDTH_HEIGHT = 100
-    
+class Block:
+    BLOCK_WIDTH_HEIGHT = 20
+    def __init__(self, x, y, blockImg):
+        self.x = x
+        self.y = y
+        self.blockImg = pygame.transform.scale(blockImg, (self.BLOCK_WIDTH_HEIGHT, self.BLOCK_WIDTH_HEIGHT))
+
+    def _GetCenter(self, x, y, surface):
+        return (x - surface.get_width() // 2, y - surface.get_width() // 2)
+
+    def Draw(self, display):
+        display.blit(self.blockImg, self._GetCenter(self.x, self.y, self.blockImg))
